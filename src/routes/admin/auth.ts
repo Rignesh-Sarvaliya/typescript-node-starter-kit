33b7674248818c2c2a46adc9b545d9102b2e1bdb
Rignesh Sarvaliya <rignesh@excellentwebworld.in>
2025-07-07 18:53:11 +0530

    refactor: rename admin login function for consistency

--- src/routes/admin/auth.ts
+++ src/routes/admin/auth.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { adminLogin } from "./auth.controller";
+import { loginAdmin } from "./auth.controller";
 import { AdminLoginRequestSchema } from "../../requests/admin/auth.request";
 import validateRequest from "../../middlewares/validateRequest";
 import { logRoute } from "../../decorators/logRoute";
@@ -10,7 +10,7 @@ router.post(
   "/admin/login",
   logRoute("ADMIN_LOGIN"),
   validateRequest({ body: AdminLoginRequestSchema }),
-  adminLogin
+  loginAdmin
 );
 
 export default router;
