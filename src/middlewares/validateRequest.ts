a5ce6b0a3c656a0a2c2b5443aa5af85d218fc434
Rignesh-EWW <86728945+Rignesh-EWW@users.noreply.github.com>
2025-07-08 13:05:53 +0530

    fix: avoid variable name conflict in validateRequest

--- src/middlewares/validateRequest.ts
+++ src/middlewares/validateRequest.ts
@@ -18,11 +18,11 @@ export default function validateRequest({
       if (query) req.query = query.parse(req.query);
       if (params) req.params = params.parse(req.params);
       next();
-    } catch (error) {
-      if (error instanceof ZodError) {
+    } catch (err) {
+      if (err instanceof ZodError) {
         return res
           .status(422)
-          .json(error("Validation failed", formatZodError(error)));
+          .json(error("Validation failed", formatZodError(err)));
       }
 
       return res.status(500).json(error("Unexpected validation error"));
